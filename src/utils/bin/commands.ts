f32277eaa34f80cb1911f48080efcbdfb61ec6eb
Peter Huang <peter.huang@neofinancial.com>
2022-11-26 18:18:20 -0700

    update theme and minor command update

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -100,8 +100,7 @@ directories`;
 };
 
 export const cd = async (args: string[]): Promise<string> => {
-  return `unfortunately, i cannot afford more directories.
-if you want to help, you can type 'donate'.`;
+  return `Let's just stick with the current directory.`;
 };
 
 export const date = async (args: string[]): Promise<string> => {
